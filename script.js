a761c5a7b4532d03f33b7f5a8a4747d632e2aa72
Oxana <62831704+oxyrud@users.noreply.github.com>
2020-08-28 19:15:09 +0300

    changed index, js

--- script.js
+++ script.js
@@ -5,10 +5,32 @@ const selectLevel = (item) => {
 
 };
 
-level.addEventListener('click',selectLevel );
+level.forEach((item) => item.addEventListener('click',selectLevel ));
 
-function createGameField(){
+function showLevel() {
+	let activeLevel = document.querySelector('active');
+	let idActiveLevel = activeLevel.id;
+	return idActiveLevel;
+}
 
+function getNumberOfCard () {
+	const idActiveLevel = showLevel();
+	let numberOFCards = 0;
+	switch(idActiveLevel){
+		case "easy":
+		    numberOFCards = 3;
+		break;
+		case "medium":
+		    numberOFCards = 6;
+		break;
+		default:
+		    numberOFCards = 9;
+	}
+    return numberOFCards;
+
+}
+
+function createGameTable(){
 
 }
 
