0ea78025a6ed8d738bdd3743161b09ecc694f018
Oxana <62831704+oxyrud@users.noreply.github.com>
2020-09-07 16:50:21 +0300

    changed style,js

--- script.js
+++ script.js
@@ -13,29 +13,27 @@ function showLevel() {
 	let idActiveLevel = activeLevel.id;
 	return idActiveLevel;
 }
-
+//получение количества карт в зависимости от уровня игры
 function getNumberOfCard () {
 	const idActiveLevel = showLevel();
-	let numberOFCards = 0;
+	let numberOfCards = 0;
 	switch(idActiveLevel){
 		case "easy":
-		    numberOFCards = 3;
+		    numberOfCards = 3;
 		break;
 		case "medium":
-		    numberOFCards = 6;
+		    numberOfCards = 6;
 		break;
 		default:
-		    numberOFCards = 9;
+		    numberOfCards = 9;
 	}
-    return numberOFCards;
+    return numberOfCards;
 
 }
-
+// создание игрового стола
 function createGameTable(){
     const cards = getNumberOfCard ();
-    
-    	
-    
+       
     for (let i=0; i<cards; i++) {
     	    console.log(i);
     	    let card = document.createElement("div");
@@ -45,12 +43,32 @@ function createGameTable(){
     	    console.log(card);	
     }
 }
-debugger;
+// получение случайной карты
+function getRandomCard(){
+    const playCards = document.querySelectorAll('.gametable');
+    const gameCards = getNumberOfCard;
+    let randomCard = Math.floor(Math.random() * gameCards);
+ 
+        for (let i = 0; i < gameCards; i++ ) {
+            if (i === randomCard) { playCards[i].firstElementChild.src = "images/bug.png"; 
+            }  else {
+        	    playCards[i].firstElementChild.src = "images/gameover.png";
+            }
+}
+// переворот карты
+function flipCard() {
+	this.classList.toggle("onclik");
+	const flippedCard = document.querySelectorAll('.gametable');
+ 	flippedCard.forEach(item =>
+		item.addEventListener("click", startGame));
+}
+
 function startGame() {
     getNumberOfCard();
 	createGameTable();
     document.querySelector('.main').classList.add('hidden');
+    getRandomCard();
+    flipCard();
 }
-
+debugger;
 start.addEventListener ('click', startGame);
-
